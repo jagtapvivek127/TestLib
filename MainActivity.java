c5fe8b95ebabccc35a210dedddac1a18965673ae
jagtapvivek127 <jagtapvivek127@gmail.com>
2020-03-13 14:09:37 +0530

    Comment added for method

--- MainActivity.java
+++ MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialize();
+        initialize();// TO initialize the views
+		
     }
 }
